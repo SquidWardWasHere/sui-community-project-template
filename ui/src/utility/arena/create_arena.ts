67b416d469f3efbd31ec96d02ee8a6e11bd2582a
SquidWardWasHere <hasannalii530@gmail.com>
2025-11-14 22:54:17 +0300

    Update create_arena.ts

--- ui/src/utility/arena/create_arena.ts
+++ ui/src/utility/arena/create_arena.ts
@@ -4,11 +4,14 @@ export const createArena = (packageId: string, heroId: string) => {
   const tx = new Transaction();
   
   // TODO: Add moveCall to create a battle place
-  // Function: `${packageId}::arena::create_arena`
-  // Arguments: heroId (object)
-    // Hints:
-    // Use tx.object() for the hero object
-    // This creates a shared object that others can battle against
+  tx.moveCall({
+    target: `${packageId}::arena::create_arena`,
+    arguments: [
+      // 1. Argüman: heroId
+      // heroId bir Object ID olduğu için tx.object() ile referans edilmelidir.
+      tx.object(heroId),
+    ],
+  });
   
   return tx;
 };
